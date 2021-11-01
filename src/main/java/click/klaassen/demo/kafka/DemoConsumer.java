b505bbbe02839b1b1198c4a34a5c6a393900b8c5
Marco Klaassen <mklaasse@redhat.com>
2021-11-01 20:08:13 +0100

    add endpoint feature to consumer

--- src/main/java/click/klaassen/demo/kafka/DemoConsumer.java
+++ src/main/java/click/klaassen/demo/kafka/DemoConsumer.java
@@ -1,15 +1,26 @@
 package click.klaassen.demo.kafka;
 
-import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.jboss.resteasy.annotations.SseElementType;
+import org.reactivestreams.Publisher;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
-@ApplicationScoped
+@Path("/consumer")
 public class DemoConsumer {
 
-    @Incoming("demo-topic-in")
-    public void consume(String message) {
-        System.out.println(message);
-    }
+    @Inject
+    @Channel("demo-topic-in")
+    Publisher<String> messages;
 
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @SseElementType("text/plain")
+    public Publisher<String> stream() {
+        return messages;
+    }
 }
